ba32133fe8b3e2e0d5751fc593149926a3fa8020
shuji <shuji.igarashi@gmail.com>
2024-01-05 12:51:34 +0900

    ぴよ

--- js/common.js
+++ js/common.js
@@ -77,6 +77,20 @@ document.addEventListener("DOMContentLoaded", function () {
   }
 
   /* ============================
+  // Smooth scrolling to section
+  ============================ */
+  document.querySelectorAll(".works-button").forEach(anchor => {
+    anchor.addEventListener("click", function (e) {
+      e.preventDefault();
+
+      document.querySelector(this.getAttribute("href")).scrollIntoView({
+        behavior: "smooth"
+      });
+    });
+  });
+
+
+  /* ============================
   // Testimonials Slider
   ============================ */
   if (document.querySelector(".my-slider")) {
